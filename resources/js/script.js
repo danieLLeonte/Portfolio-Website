7d036db3b98c6b7538ba96f30431340656757c1d
Dani <leonte.daniel37@gmail.com>
2022-08-09 14:51:12 +0300

    Add final touches for version 1.0

---
+++ resources/js/script.js
@@ -0,0 +1,20 @@
+// Contact Button
+document.getElementById("contact-bt").onclick = function () {
+  location.href = "./contact.html";
+};
+
+// Git Show Code Button
+document.getElementsByClassName('github-bt')[0].onclick = function () {
+  window.open('https://github.com/danieLLeonte/Codecademy-Mixed-Messages');
+};
+
+const sections = document.querySelectorAll('section');
+sections.forEach(section => {
+  section.addEventListener('mouseenter', (event) => {
+    // section.style.fontSize = "30px";
+
+    // setTimeout(() => {
+    //   section.style.fontSize = "";
+    // }, 500);
+  })
+});
